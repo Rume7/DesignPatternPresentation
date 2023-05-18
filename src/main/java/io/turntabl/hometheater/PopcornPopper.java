a4256f6e6b3e463676a05ac5c5ced1212d4a684f
RhumeLeap <rhume.disi@turntabl.io>
2023-05-18 05:27:12 +0100

    PopcornPopper class completed and tested ok.

---
+++ src/main/java/io/turntabl/hometheater/PopcornPopper.java
@@ -0,0 +1,13 @@
+package io.turntabl.hometheater;
+
+public class PopcornPopper extends ElectronicDevice{
+
+    public PopcornPopper(String name) {
+        super(name);
+    }
+
+    public void pop() {
+        System.out.println("Kindly have some popcorn");
+    }
+
+}
